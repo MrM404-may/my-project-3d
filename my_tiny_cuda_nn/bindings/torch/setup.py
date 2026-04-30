91be62d9729b010ddc8f7ee247ae5ac1b531da6c
MrM404-may <MrM404-may@users.noreply.github.com>
2026-04-30 09:06:22 +0000

    Revert commits b4ddcb6..b4ddcb6
    This reverts 1 commit(s) from b4ddcb6 to b4ddcb6.
    
    Reverted commits:
    b4ddcb6 feat: 了解 Instant-NGP 哈希表实现
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- my_tiny_cuda_nn/bindings/torch/setup.py
+++ my_tiny_cuda_nn/bindings/torch/setup.py
@@ -2,10 +2,7 @@ import os
 
 import re
 from setuptools import setup
-try:
-    from packaging.version import parse as parse_version
-except ImportError:
-    from pkg_resources import parse_version
+from pkg_resources import parse_version
 import subprocess
 import shutil
 import sys
@@ -80,8 +77,8 @@ if os.name == "nt":
 		os.environ["PATH"] += ";" + cl_path
 	else:
 		# cl.exe was found in PATH, so we can assume that the user is already in a developer command prompt
-		# In this case, BuildExtensions requires the following environment variable to be set such that
-		# it won't try to activate a developer command prompt a second time.
+		# In this case, BuildExtensions requires the following environment variable to be set such that it
+		# won't try to activate a developer command prompt a second time.
 		os.environ["DISTUTILS_USE_SDK"] = "1"
 
 cpp_standard = 14
